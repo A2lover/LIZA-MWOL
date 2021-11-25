8d82bf326790259955690298f83eef08e399ef49
A2lover <94605771+A2lover@users.noreply.github.com>
2021-11-25 23:43:24 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -133,7 +133,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
         });
 
         console.log(
-            chalk.green.bold('✅ Liza Mwol working!')
+            chalk.green.bold('✅ Sha_Bot-2 working!')
         );
         await new Promise(r => setTimeout(r, 1100));
 
